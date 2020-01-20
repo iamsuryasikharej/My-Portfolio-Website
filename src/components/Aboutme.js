8abd6366d292da827dfea34e877146bebd576241
iamsuryasikharej <iamssrofficial@gmail.com>
2020-01-20 18:44:08 +0530

    contact me page done

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import {Grid,Cell} from 'react-mdl'
 function Aboutme() {
     return (
-        <div>
-            <h1>About me</h1>
-        </div>
+        <div>abt me</div>
     )
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import { Grid, Cell,List,ListItem,ListItemContent } from 'react-mdl'
 function Contact() {
     return (
-        <div>
-            <h1>contact</h1>
+        <div className="contactPage-style">
+            <Grid className="grid-style">
+                <Cell col={6}>
+                <h2 className="myname-style">Surya Sikha Rej</h2>
+                <img src="https://cdn1.iconfinder.com/data/icons/man-profession-2/64/coder-job-hire-avatar-512.png" style={{height:"200px"}}></img>
+                <h1> Hey There,
+                Surya here. I am a fresh garduate looking for a role as a software developer.I am passionate coder and problem solver.I love learning new technologies and implementing them in projects. Please do check my git-hub.</h1>
+                </Cell>
+                <Cell col={6}><h2>Contact Me</h2>
+                <hr></hr>
+                <div className="list-style">
+                    <List >
+                            <ListItem>
+                            <ListItemContent style={{fontSize:"25px" , fontFamily:'Anton'}}>
+                            <i class="fas fa-phone-square-alt">     (+91) 7605855625</i></ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{ fontSize: "25px", fontFamily: 'Anton' }}><i class="fas fa-at">        iamssrofficial@gmail.com</i></ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{ fontSize: "25px", fontFamily: 'Anton' }}><i class="fas fa-map-marker-alt">            India</i></ListItemContent>
+                            </ListItem>
+                    </List>
+                </div>
+                </Cell>
+            </Grid>
         </div>
     )
 }
